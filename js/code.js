0f932aa74941168a6b541608f951c3972b146cb6
Albert Buendia <6235.joan23@fje.edu>
2020-11-30 20:46:17 +0100

    first commit

--- js/code.js
+++ js/code.js
@@ -1,17 +1,18 @@
+window.onload=function(){
+    modal=document.getElementById('myModal');
+}
+
 // When the user clicks anywhere outside of the modal, close it
 window.onclick = function(event) {
-    let modal=document.getElementById('myModal');
     if (event.target == modal) {
       modal.style.display = "none";
     }
   }
 
 function openModal(){
-    let modal=document.getElementById('myModal');
     modal.style.display = "block";
 }
 
 function closeModal(){
-    let modal=document.getElementById('myModal');
     modal.style.display = "none";
 }
